d98ef2eb26dcf4df5ba6d574e9f7aded4f04f13f
B Terbush <brittanyterbush@gmail.com>
2018-01-05 14:24:09 -0800

    fix db schema bugs.

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS contacts;
 DROP TABLE IF EXISTS users;
+CREATE TABLE session;
 
 CREATE TABLE contacts (
   id serial,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL
-)
+);
 
 CREATE TABLE users (
   id serial,
@@ -13,10 +14,10 @@ CREATE TABLE users (
   email  VARCHAR(255) NOT NULL,
   password  VARCHAR(255) NOT NULL,
   role VARCHAR DEFAULT 'regular'
-)
+);
 
 CREATE TABLE session (
   "sid" VARCHAR NOT NULL,
   "sess" JSON NOT NULL,
   "expire" TIMESTAMP(6) NOT NULL
-)
+);
